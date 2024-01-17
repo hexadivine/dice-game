cd5f8e64201f90b2a2128a7752fc80ab2b3f1dab
hexadivine <hexadivine@gmail.com>
2024-01-17 11:42:16 +0530

    keeping dice game

--- project3_dicegame/src/components/styled/GameRules.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const GameComponent = styled.div`
-    max-width: 754px;
-    height: 166px;
-    padding: 20px 50px;
-    background-color: #fbf1f1;
-`;
--- project3_dicegame/src/pages/styled/HomePage.module.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Interact = styled.div`
-    h1 {
-        font-size: 96px;
-        font-weight: 800;
-    }
-`;
